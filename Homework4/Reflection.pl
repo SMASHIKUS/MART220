e160142d3e65c1439d9c3d5daf9504ec584ddec0
SMASHIKUS <aj223465@umconnect.umt.edu>
2026-02-09 14:37:57 -0700

    Create Reflection.pl

---
+++ Homework4/Reflection.pl
@@ -0,0 +1,5 @@
+    I used the Flying Spaghetti Monster from Futurama because it's one of my favorite shows and the character has a really
+    silly design. I wanted it to feel like you were chasing the plate around the screen in a playful "turning of the tables"
+    kind of way. I could see innevitably adding collision and maybe some kind of score tracker so that every time the player
+    catches the plate, they get a point. I can see how displaying images this way could be really useful for all kinds of projects.
+    you could easily make those images cycle using an array to create simple animations.
