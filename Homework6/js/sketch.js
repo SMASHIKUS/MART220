1c48e9a7cef8519437e5f2ddd114c6cf94618fb9
SMASHIKUS <aj223465@umconnect.umt.edu>
2026-02-23 21:35:15 -0700

    Homework 6
    Homework 6

---
+++ Homework6/js/sketch.js
@@ -0,0 +1,105 @@
+//Twinstick Zombie Shooter Demo by Adam Jackson
+
+//Variables
+
+let playerImage;
+
+let playerX = 0;
+let playerY = 0;
+
+let playerIdle = [];
+let playerWalk = [];
+
+let foodImages = [];
+let foodItems = [];
+
+let weaponImages = [];
+let weaponItems = [];
+
+let playerSpeed = 1;
+let currentFrame = 0;
+let frameCounter = 0;
+let frameDelay = 20;
+
+//Note add later: 
+  // let playerHealth = 100;
+  // let zombieIdle = [];
+  // let zombieX = 0;
+  // let zombieY = 0;
+  // let zombieImage;
+  // let zombieWalk = [];
+  // let zombieSpeed = 2;
+  // Fix camera to player
+
+function preload() {
+  for (let i = 0; i < 6; i++) {
+    playerIdle[i] = loadImage("assets/images/CharacterSprites/Idle (" + (i + 1) + ").png");
+    playerWalk[i] = loadImage("assets/images/CharacterSprites/Walk (" + (i + 1) + ").png");
+  }
+  
+  for (let i = 0; i < 3; i++) {
+    foodImages[i] = loadImage("assets/images/Food/Food (" + (i + 1) + ").png");
+    weaponImages[i] = loadImage("assets/images/Weapons/Weapon (" + (i + 1) + ").png");
+  }
+}
+
+function setup() {
+  createCanvas(400, 400);
+  
+  for (let i = 0; i < 3; i++) {
+    spawnFood();
+  }
+
+  for (let i = 0; i < 3; i++) {
+    spawnWeapon();
+  }
+}
+
+function spawnFood() {
+  let food = Food.spawn(foodImages);
+  foodItems.push(food);
+}
+
+function spawnWeapon() {
+  let weapon = Weapon.spawn(weaponImages);
+  weaponItems.push(weapon);
+}
+
+function draw() {
+  background(220);
+  translate(-playerX + width/2, -playerY + height/2);
+  
+  for (let i = 0; i < foodItems.length; i++) {
+    foodItems[i].display();
+  }
+
+  for (let i = 0; i < weaponItems.length; i++) {
+    weaponItems[i].display();
+  }
+
+  frameCounter++;
+  if (frameCounter >= frameDelay) {
+    frameCounter = 0;
+    currentFrame = (currentFrame + 1) % playerWalk.length;
+  }
+
+  //Player Movement
+  if (keyIsPressed) {
+    if (key == "a") {
+      playerX -= playerSpeed;
+    }
+    if (key == "w") {
+      playerY -= playerSpeed;
+    }
+    if (key == "d") {
+      playerX += playerSpeed;
+    }
+    if (key == "s") {
+      playerY += playerSpeed;
+    }
+    image(playerWalk[currentFrame], playerX, playerY);
+  }
+  else {
+    image(playerIdle[currentFrame], playerX, playerY);
+  }
+}
